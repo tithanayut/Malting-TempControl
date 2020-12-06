9e0e411681773434c6db9204611ccbe4dd097a94
Thanayut T <30551284+tithanayut@users.noreply.github.com>
2020-12-06 14:53:27 +0700

    Update sketch_dec05a.ino

--- sketch_dec05a/sketch_dec05a.ino
+++ sketch_dec05a/sketch_dec05a.ino
@@ -1,17 +1,30 @@
 #include "DHT.h"
+#include "ESP8266WiFi.h"
+#include "ESP8266HTTPClient.h"
 
-#define DHTPIN 6
+#define DHTPIN 2
 #define DHTTYPE DHT11 // DHT 11 
+
+const char* ssid = "SSID"; //WiFi SSID
+const char* password = "PASSWORD"; //WiFi Password
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+  pinMode(D1, OUTPUT);
+  
   Serial.begin(9600); 
   dht.begin();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting..");
+  }
 }
 
 void loop() {
-  float t = dht.readTemperature();
+  int t = dht.readTemperature();
   
   if (isnan(t)){
     Serial.println("Failed to read from DHT");
@@ -19,5 +32,31 @@ void loop() {
     Serial.print("Temperature: "); 
     Serial.print(t);
     Serial.println(" *C");
+
+    //REPORT
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      String apicall = "http://SERVERADDRESS/malting/api.php?temp=" + String(t); //API CALL ADDRESS
+      Serial.println(apicall);
+      http.begin(apicall);
+      int httpCode = http.GET();   
+ 
+      if (httpCode > 0) { 
+        String payload = http.getString();
+        Serial.print("HTTP API Reply: "); 
+        Serial.println(payload);                  
+      }
+      http.end();
+    }
+
+
+    //RELAY
+    if(t >= 16){
+      digitalWrite(D1, HIGH);
+    }else{
+      digitalWrite(D1, LOW);
+    }
+      
+    delay(10000);
   }
 }
